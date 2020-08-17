862b068d967f9e1bf65e8342f107df260b00eb2c
Bohdan <bodyanu4killer1@gmail.com>
2020-08-17 19:34:08 +0300

    Add POST, Like, Delete, Star

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import './app-header.css';
+
+
+
+
+const AppHeader = ({liked, allPosts}) => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Bohdan Strukov</h1>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
+
+        </div>
+    )
+}
+
+export default AppHeader;
